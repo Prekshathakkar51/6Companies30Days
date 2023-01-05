3ffbd92232f7444c0abd8e53b16b438c05423377
Prekshathakkar51 <95307102+Prekshathakkar51@users.noreply.github.com>
2023-01-06 02:46:57 +0530

    Update solution.java

--- Microsoft/1227. Airplane Seat Assignment Probability/solution.java
+++ Microsoft/1227. Airplane Seat Assignment Probability/solution.java
@@ -1,10 +1,11 @@
 
- class Solution{
+class Solution{
     public double nthPersonGetsNthSeat(int n){
-        if(n==1){
+        if(n==1)
             return 1;
-            else return 0.5;
+        
+        else return 0.5;
             
         }
     }
- }
+ 
